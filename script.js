f1946b0438c8b8d2e59b46e0de5ce17706ec5f16
Chris Finney <chris.finney@pantheon.io>
2022-03-15 01:36:42 -0500

    displaying the correct images

--- script.js
+++ script.js
@@ -19,16 +19,29 @@ async function getDates() {
 
 async function render(){
     let dates = await getDates();
-    console.log(dates);
+    
     let html = "";
+    
+    let today = new Date();
+    let todaysDate = today.toISOString().split("T")[0]
+    console.log(todaysDate)
     dates = dates.homeGames;
-    dates.forEach(date => {
-    let p = `<p>${date}</p>`;
-    html += p;    
-    })
-    let container = document.querySelector('.container');
-    container.innerHTML = html;
-    return dates;
+
+    const isHomeGame = dates.includes(todaysDate);
+    console.log(isHomeGame)
+    let header = document.querySelector('.header');
+    let imageBox = document.querySelector('.main')
+    let headline;
+
+    if(!isHomeGame){
+        headline = `Hey y'all the Baron's ain't playin tonight. Traffic is gonna be a-ok!`;
+        header.innerHTML = headline;
+        imageBox.style.background = "url('assssets/nogame.png') no-repeat center center fixed"
+    } else {
+        headline = `Traffic is all jacked up cause the Baron's are playing`;
+        header.innerHTML = headline;
+        imageBox.style.background = "url('assssets/play_ball.jpeg') no-repeat center top fixed"
+    }
 }
 render();
 let promiseResolve = render();
